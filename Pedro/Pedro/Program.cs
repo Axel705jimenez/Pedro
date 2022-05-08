fd0a7fd18866548695c13b5e1ea17797c2f6df47
Axel705jimenez <70666416+Axel705jimenez@users.noreply.github.com>
2022-05-08 16:46:32 -0500

    Update Program.cs

--- Pedro/Pedro/Program.cs
+++ Pedro/Pedro/Program.cs
@@ -16,7 +16,7 @@ namespace Pedro
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Minecraft());
+            Application.Run(new Form1());
         }
     }
 }
